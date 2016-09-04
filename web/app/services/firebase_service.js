6dbdd8b6e49875f6c41cc926ca38d61a9d58ab49
arkuar <arttukuronen@gmail.com>
2016-09-04 13:22:45 +0300

    Säätämistä

---
+++ web/app/services/firebase_service.js
@@ -0,0 +1,27 @@
+MovieApp.service('FirebaseService', function($firebaseArray) {
+    var firebaseRef = new Firebase('https://elokuvakirjasto-446d1.firebaseio.com/movies');
+    var movies = $firebaseArray(firebaseRef);
+    
+    this.getMovies = function() {
+        return movies;
+    };
+    
+    this.addMovie = function(movie) {
+        movies.$add(movie);
+    };
+    
+    this.getMovie = function(key, done){
+        movies.$loaded(function(){
+            done(movies.$getRecord(key));
+        });
+    };
+    
+    this.removeMovie = function(movie){
+        movies.$remove(movie);
+    };
+    
+    this.editMovie = function(movie){
+        movies.$save(movie);
+    };
+    
+});
